f7d8c255b729b2a4cc4c776af585a4e168e19f68
Lauren Boyer <lboyer4@outlook.com>
2019-01-31 08:31:37 -0700

    First JavaScript on file

--- guessnumber.main.js
+++ guessnumber.main.js
@@ -1,6 +1,15 @@
-var actualNumber
-Math.floor(Math.random()*10)+1
+var randomNumber = null;
 
+function setDefaults () { 
+var min= 1
+var max=100
+makeRandomNumber(min, max)
+}
 
-
-if()
+function makeRandomNumber(min, max) {
+min = Math.ceil(min);
+max = Math.floor(max);
+randomNumber = Math.floor(Math.random() * (max - min)) + min;
+console.log('what is my random number?' , randomNumber)
+return randomNumber
+}
