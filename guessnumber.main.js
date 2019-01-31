27d5f5d2a4e03adaca962dc476bc11a5dca099f3
Lauren Boyer <lboyer4@outlook.com>
2019-01-31 13:11:47 -0700

    changes to range in javascript

--- guessnumber.main.js
+++ guessnumber.main.js
@@ -1,15 +1,26 @@
 var randomNumber = null;
+// var min;
+// var max;
 
-function setDefaults () { 
-var min= 1
-var max=100
-makeRandomNumber(min, max)
+function setDefaults() { 
+	makeRandomNumber();
 }
 
-function makeRandomNumber(min, max) {
-min = Math.ceil(min);
-max = Math.floor(max);
-randomNumber = Math.floor(Math.random() * (max - min)) + min;
-console.log('what is my random number?' , randomNumber)
-return randomNumber
+function makeRandomNumber(min = 1, max = 100) {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	randomNumber = Math.floor(Math.random() * (max - min)) + min;
+	console.log('what is my random number?' , randomNumber);
+	return randomNumber;
 }
+
+function buttonSetRange(evt) {
+	var minInput = document.getElementById('min');
+	var maxInput = document.getElementById('max');
+	if (min != null && max != null) {
+		makeRandomNumber(minInput.value.toString(), maxInput.value.toString());
+		console.log(document.querySelector('.low-range').innerText);
+		document.querySelector('.low-range').innerText = minInput.value;
+		document.querySelector('.high-range').innerText = maxInput.value;
+	}
+} 
