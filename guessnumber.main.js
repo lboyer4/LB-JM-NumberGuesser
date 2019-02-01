51b4ab752982b53d360777b0b5d1dbb3c50a9cd6
Jacqueline Moore <jacquelinebmoore@gmail.com>
2019-02-01 09:31:57 -0700

    finished cards

--- guessnumber.main.js
+++ guessnumber.main.js
@@ -1,6 +1,16 @@
 var actualNumber
 Math.floor(Math.random()*10)+1
 
+var nameInputOne = document.querySelector('#name-input-one');
+var nameInputTwo = document.querySelector('#name-input-two');
+var nameButton = document.querySelector('#guess-button');
+var challengerNameOne = document.querySelector('#challenger-one');
+var challengerNameTwo = document.querySelector('#challenger-two');
 
+nameButton.addEventListener('click', renameFunction);
 
-if()
+function renameFunction() {
+  challengerNameOne.innerText = nameInputOne.value;
+  challengerNameTwo.innerText = nameInputTwo.value;
+}
+
