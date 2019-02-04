9fa82fe3788c461768cf7846034bc476d04fdf41
Lauren Boyer <lboyer4@outlook.com>
2019-02-03 19:50:45 -0700

    Javascript with incomplete number conditional

--- guessnumber.main.js
+++ guessnumber.main.js
@@ -1,28 +1,11 @@
 var randomNumber = null;
-// var min;
-// var max;
-
-<<<<<<< HEAD
-var nameInputOne = document.querySelector('#name-input-one');
-var nameInputTwo = document.querySelector('#name-input-two');
-var nameButton = document.querySelector('#guess-button');
-var challengerNameOne = document.querySelector('#challenger-one');
-var challengerNameTwo = document.querySelector('#challenger-two');
-
-nameButton.addEventListener('click', renameFunction);
-
-function renameFunction() {
-  challengerNameOne.innerText = nameInputOne.value;
-  challengerNameTwo.innerText = nameInputTwo.value;
-}
-=======
 function setDefaults() { 
 	makeRandomNumber();
 }
 
 function makeRandomNumber(min = 1, max = 100) {
-	min = Math.ceil(min);
-	max = Math.floor(max);
+	var min = Math.ceil(min);
+	var max = Math.floor(max);
 	randomNumber = Math.floor(Math.random() * (max - min)) + min;
 	console.log('what is my random number?' , randomNumber);
 	return randomNumber;
@@ -39,6 +22,11 @@ function buttonSetRange(evt) {
 	}
 } 
 
+function makeGuesses () {
+	var changeThis = document.getElementById('current-guess-one').value;
+	return changeThis = 5;
+}
+
 function makeGuesses() {
 	var playerOneGuess = document.getElementById('guess-one').value;
 	var playerTwoGuess = document.getElementById('guess-two').value;
@@ -46,21 +34,38 @@ function makeGuesses() {
 	var playerOneText= document.getElementById('guesser-one-text').innerText;
 	var playerTwoText= document.getElementById('guesser-two-text').innerText;
 
+	console.log(playerOneText);
+	console.log(playerTwoText);
 	// var playerOneText.innerText = playerOneGuess;
  //  var playerTwoText.innerText = playerTwoGuess;
 
-if (playerOneGuess > randomNumber) {
-	playerOneText = 'Too High';
-} else if (playerOneGuess < randomNumber) { 
-	playerOneText = 'Too Low';
-} else { playerOneText = 'Correct!';
+	if (playerOneGuess > randomNumber) {
+		playerOneText.innerText = 'Too High';
+	} else if (playerOneGuess < randomNumber) { 
+		playerOneText = 'Too Low';
+	} else { playerOneText.innerText = 'Correct!';
+	}
+
+	if (playerTwoGuess > randomNumber) {
+		playerTwoText.innerText = 'Too High';
+	} else if (playerTwoGuess < randomNumber) {
+	 playerTwoText ='Too Low';
+	} else { playerTwoText.innerText = 'Correct!';
+	}
 }
 
-if (playerTwoText > randomNumber) {
-	playerTwoText = 'Too High';
-} else if (playerTwoGuess < randomNumber) {
- playerTwoText ='Too Low';
-} else { playerTwoText = 'Correct!';
-}}
->>>>>>> 12b9ab08ca8e0f6309212408e3d5634d3d6c7ee1
+
+
+// var nameInputOne = document.querySelector('#name-input-one');
+// var nameInputTwo = document.querySelector('#name-input-two');
+// var nameButton = document.querySelector('#guess-button');
+// var challengerNameOne = document.querySelector('#challenger-one');
+// var challengerNameTwo = document.querySelector('#challenger-two');
+
+// nameButton.addEventListener('click', renameFunction);
+
+// function renameFunction() {
+//   challengerNameOne.innerText = nameInputOne.value;
+//   challengerNameTwo.innerText = nameInputTwo.value;
+// }
 
